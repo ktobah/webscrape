9e8a1954e3dc1e75e4a5a0d5443aa7d8f510d99e
Ahmed Ktob <ktobah@gmail.com>
2018-11-06 17:36:15 +0100

    Webscraper initial commit

---
+++ src/main/java/DecodeCaptcha.java
@@ -0,0 +1,28 @@
+import org.bytedeco.javacpp.BytePointer;
+
+import static org.bytedeco.javacpp.lept.*;
+import static org.bytedeco.javacpp.tesseract.TessBaseAPI;
+
+public class DecodeCaptcha {
+
+    public static String decodeCaptcha(String imageCaptcha) {
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+
+        api.Init("/usr/local/Cellar/tesseract/4.0.0/share/tessdata", "eng");
+
+        // Open input image with leptonica library
+        PIX image =  pixRead(imageCaptcha);
+        api.SetImage(image);
+        // Get OCR result
+        outText = api.GetUTF8Text();
+
+        // Destroy used object and release memory
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+
+        return outText.getString();
+    }
+}
